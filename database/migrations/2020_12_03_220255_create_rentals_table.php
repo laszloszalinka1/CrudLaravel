48ea3da17a396174664e5fb20f92ee53dad4ba0e
laszloszalinka1 <69268658+laszloszalinka1@users.noreply.github.com>
2020-12-04 00:31:45 -0500

    Add files via upload

---
+++ database/migrations/2020_12_03_220255_create_rentals_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRentalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rentals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->integer('total');
+            $table->integer('user_id');
+            $table->integer('status_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rentals');
+    }
+}
